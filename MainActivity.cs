c4f912e3806a83b647fbea258f701a8bbd0077eb
yuviblue <55740646+yuviblue@users.noreply.github.com>
2019-12-30 21:07:01 +0200

    Added "+" button and Dialog, created SearchActivity.cs

--- MainActivity.cs
+++ MainActivity.cs
@@ -16,18 +16,14 @@ namespace QuickChef
 {
     [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
     public class MainActivity : AppCompatActivity
-    {
-        EditText et1, et2;
-        Button btn;
+    { 
+        EditText etSearch;
+        Button btnConfirm;
+        Button btnSearch;
+        Button btnAdd;  
+        ListView lvIngridients;
+        Dialog d;
         
-        
-        ListView lv;
-        SearchAdapter searchAdapter;
-        private const string apiKey = "158e67089b3f42e3b3d6a3cb512213b7";
-        private const string baseUrl = "https://api.spoonacular.com/recipes/";
-        private const string searchApi = "findByIngredients";
-        private const int recipeCount = 5;
-
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
@@ -35,64 +31,33 @@ namespace QuickChef
             // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.activity_main);
 
-            et1 = FindViewById<EditText>(Resource.Id.editText1);
-            et2 = FindViewById<EditText>(Resource.Id.editText2);
-            btn = FindViewById<Button>(Resource.Id.button1);
-            
-            btn.Click += Btn_Click;
+            btnSearch = FindViewById<Button>(Resource.Id.btnSearch);
+            btnAdd = FindViewById<Button>(Resource.Id.btnAdd);
+
+            btnSearch.Click += BtnSearch_Click;
+            btnAdd.Click += BtnAdd_Click;
         }
 
-        private void Btn_Click(object sender, System.EventArgs e)
+        private void BtnAdd_Click(object sender, EventArgs e)
         {
-            StartSearch();
+            AddIngridientDialog();
         }
 
-        private void StartSearch()
+        private void AddIngridientDialog()
         {
-            string url = $"{baseUrl}{searchApi}?apiKey={apiKey}&ingredients=apples,+flour,+sugar&number={recipeCount}&includeInstructions=true";
-            List<ShortRecipe> result = RequestHandler<List<ShortRecipe>>.GetDataFromRequest(url);
-
-            List<Entry> recipes = new List<Entry>();
-
-            if (result != null)
-            {
-                foreach (ShortRecipe v in result)
-                {
-                    var entry = new Entry()
-                    {
-                        Picture = GetImageBitmapFromUrl(v.image),
-                        Title = v.title,
-                        Id = v.id
-                    };
-
-                    recipes.Add(entry);
-                }
-
-                var adapter = new SearchAdapter(this, recipes);
-                lv = FindViewById<ListView>(Resource.Id.lv);
-                lv.Adapter = adapter;
-
-            }
+            d = new Dialog(this);
+            d.SetContentView(Resource.Layout.dialog_search_layout);
+            d.SetTitle("Ingridient");
+            d.SetCancelable(true);
+            etSearch = d.FindViewById<EditText>(Resource.Id.etSearch);
+            btnConfirm = d.FindViewById<Button>(Resource.Id.btnConfirm);
+            d.Show();
         }
 
-        private Bitmap GetImageBitmapFromUrl(string url)
+        private void BtnSearch_Click(object sender, System.EventArgs e)
         {
-            Bitmap imageBitmap = null;
-
-            using (var webClient = new System.Net.WebClient())
-            {
-                var imageBytes = webClient.DownloadData(url);
-                if (imageBytes != null && imageBytes.Length > 0)
-                {
-                    imageBitmap = BitmapFactory.DecodeByteArray(imageBytes, 0, imageBytes.Length);
-                }
-            }
-
-            return imageBitmap;
+            //StartSearch();
         }
-
-
-
 
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
         {
---
+++ SearchActivity.cs
@@ -0,0 +1,76 @@
+﻿ using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.Graphics;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace QuickChef
+{
+    [Activity(Label = "SearchActivity")]
+    public class SearchActivity : Activity
+    {
+        ListView lvRecipes;
+        private const string apiKey = "158e67089b3f42e3b3d6a3cb512213b7";
+        private const string baseUrl = "https://api.spoonacular.com/recipes/";
+        private const string searchApi = "findByIngredients";
+        private const int recipeCount = 5;
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Create your application here
+        }
+
+        private void StartSearch()
+        {
+            string url = $"{baseUrl}{searchApi}?apiKey={apiKey}&ingredients=apples,+flour,+sugar&number={recipeCount}&includeInstructions=true";
+            List<ShortRecipe> result = RequestHandler<List<ShortRecipe>>.GetDataFromRequest(url);
+
+            List<Entry> recipes = new List<Entry>();
+
+            if (result != null)
+            {
+                foreach (ShortRecipe v in result)
+                {
+                    var entry = new Entry()
+                    {
+                        Picture = GetImageBitmapFromUrl(v.image),
+                        Title = v.title,
+                        Id = v.id
+                    };
+
+                    recipes.Add(entry);
+                }
+
+                var adapter = new SearchAdapter(this, recipes);
+                lvRecipes = FindViewById<ListView>(Resource.Id.lv);
+                lvRecipes.Adapter = adapter;
+
+            }
+        }
+
+        private Bitmap GetImageBitmapFromUrl(string url)
+        {
+            Bitmap imageBitmap = null;
+
+            using (var webClient = new System.Net.WebClient())
+            {
+                var imageBytes = webClient.DownloadData(url);
+                if (imageBytes != null && imageBytes.Length > 0)
+                {
+                    imageBitmap = BitmapFactory.DecodeByteArray(imageBytes, 0, imageBytes.Length);
+                }
+            }
+
+            return imageBitmap;
+        }
+    }
+}
