a3654f8a400c70897c5050052b29b50bf01d949a
yuviblue <55740646+yuviblue@users.noreply.github.com>
2020-03-07 17:13:00 +0200

    Added SQLite for saving offline recipes

---
+++ DAL/DB.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using SQLite;
+
+namespace QuickChef.DAL
+{
+    public class DB
+    {
+        private static readonly string dbPath = Path.Combine(System.Environment.GetFolderPath(System.Environment.SpecialFolder.Personal), "QuickChefDB");
+
+        public static SQLiteConnection GetDbConnction()
+        {
+            SQLiteConnection db;
+            bool dbExists = File.Exists(dbPath);
+            db = new SQLiteConnection(dbPath);
+            if (!dbExists)
+            {
+                db.CreateTable(typeof(Download));
+            }
+            return db;
+        }
+    }
+}
---
+++ DAL/Download.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.Graphics;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using SQLite;
+
+namespace QuickChef.DAL
+{
+    [Table("Downloads")]
+    public class Download
+    {
+        [PrimaryKey, AutoIncrement, Column("id")]
+        public int Id { get; }
+        public string Title { get; }
+        public string Recipe { get; }
+        public Bitmap Image { get; }
+        public DateTime Date { get; }
+
+        public Download(string title, string recipe, Bitmap image)
+        {
+            Title = title;
+            Recipe = recipe;
+            Image = image;
+            Date = DateTime.Now;
+        }
+
+        public void Insert()
+        {
+            var db = DB.GetDbConnction();
+            db.Insert(this);
+        }
+
+        
+    }
+}
